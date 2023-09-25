be236e71d58bbd3785dd0477bbb5fec56b8ec60a
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2023-09-24 20:17:56 -0500

    feat: :wrench: Addd route example
    Add route example

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
 app = FastAPI()
-
+@app.get('/saludo')
+def saludo():
+    return {"msg":"Saludos Campers"}
 if __name__=="__main__":
     uvicorn.run("main:app",port=8000)
